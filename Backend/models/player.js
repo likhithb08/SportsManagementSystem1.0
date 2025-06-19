4e85ea5628c4bd2a655d74b07915d2aad034291f
likhithb08 <likhithb285@gmail.com>
2025-06-19 08:33:00 +0530

    Bcryptjs fix

--- Backend/models/player.js
+++ Backend/models/player.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 const performanceSchema = new mongoose.Schema({
   fitness: { type: Number, default: 0, min: 0, max: 100 },
